975f29c129c72c32830f98a59ec81a79cabdc133
BurpGod <97145581+sarangBurpGod@users.noreply.github.com>
2022-01-30 10:12:45 +0530

    Updated.

--- ERC721TokenContract.sol
+++ ERC721TokenContract.sol
@@ -1,16 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.4.2/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TEST is ERC721URIStorage, Ownable {
+contract TEST is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     constructor() ERC721("TEST", "TST") {}
 
-    function safeMint(address to, uint256 tokenId, string memory uri) external
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
     {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
